99267df1c49b0e11f0cdf577b207bfbae5c26449
Ádám Z. Kövér <adamzkover@gmail.com>
2024-12-30 10:00:39 +0100

    Add group S3

--- hapi/_upload-core.sh
+++ hapi/_upload-core.sh
@@ -1,3 +1,5 @@
-curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s1/MedicationKnowledge/MedicationKnowledge-Aranesp-inj-oppl-50-mikrog-sproyte.xml http://localhost:8080/fhir/MedicationKnowledge
-curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s1/MedicationKnowledge/MedicationKnowledge-Aspirin-tab-500-mg.xml http://localhost:8080/fhir/MedicationKnowledge
-curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s1/MedicationKnowledge/MedicationKnowledge-Intrarosa-vagitorie-6,5-mg.xml http://localhost:8080/fhir/MedicationKnowledge
+curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s1/MedicationKnowledge/MedicationKnowledge-Aspirin.xml http://localhost:8080/fhir/MedicationKnowledge
+curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s1/MedicationKnowledge/MedicationKnowledge-Intrarosa.xml http://localhost:8080/fhir/MedicationKnowledge
+curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s2/MedicationKnowledge/MedicationKnowledge-Aranesp.xml http://localhost:8080/fhir/MedicationKnowledge
+curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s3/MedicationKnowledge/MedicationKnowledge-Berotec-N.xml http://localhost:8080/fhir/MedicationKnowledge
+curl -X POST -H "Content-Type: application/fhir+xml" -d@../input/tests/WADAList/test-s3/MedicationKnowledge/MedicationKnowledge-Symbicort-Turbuhaler.xml http://localhost:8080/fhir/MedicationKnowledge
--- hapi/_upload.sh
+++ hapi/_upload.sh
@@ -1,7 +1,13 @@
 curl -X PUT -H "Content-Type: application/fhir+xml" -d@../output/Library-WADAListCommon.xml http://localhost:8080/fhir/Library/WADAListCommon
 curl -X PUT -H "Content-Type: application/fhir+xml" -d@../output/Library-WADAListS1.xml http://localhost:8080/fhir/Library/WADAListS1
 curl -X PUT -H "Content-Type: application/fhir+xml" -d@../output/Library-WADAListS2.xml http://localhost:8080/fhir/Library/WADAListS2
+curl -X PUT -H "Content-Type: application/fhir+xml" -d@../output/Library-WADAListS3.xml http://localhost:8080/fhir/Library/WADAListS3
 curl -X PUT -H "Content-Type: application/fhir+xml" -d@../output/Library-WADAList.xml http://localhost:8080/fhir/Library/WADAList
 
 curl -X PUT -H "Content-Type: application/fhir+xml" -d@../input/vocabulary/ValueSet/ValueSet-S1-FEST.xml http://localhost:8080/fhir/ValueSet/S1-FEST
 curl -X PUT -H "Content-Type: application/fhir+xml" -d@../input/vocabulary/ValueSet/ValueSet-S2-FEST.xml http://localhost:8080/fhir/ValueSet/S2-FEST
+curl -X PUT -H "Content-Type: application/fhir+xml" -d@../input/vocabulary/ValueSet/ValueSet-S3-FEST.xml http://localhost:8080/fhir/ValueSet/S3-FEST
+curl -X PUT -H "Content-Type: application/fhir+xml" -d@../input/vocabulary/ValueSet/ValueSet-S3A-FEST.xml http://localhost:8080/fhir/ValueSet/S3A-FEST
+curl -X PUT -H "Content-Type: application/fhir+xml" -d@../input/vocabulary/ValueSet/ValueSet-S3B-FEST.xml http://localhost:8080/fhir/ValueSet/S3B-FEST
+curl -X PUT -H "Content-Type: application/fhir+xml" -d@../input/vocabulary/ValueSet/ValueSet-S3C-FEST.xml http://localhost:8080/fhir/ValueSet/S3C-FEST
+curl -X PUT -H "Content-Type: application/fhir+xml" -d@../input/vocabulary/ValueSet/ValueSet-S3D-FEST.xml http://localhost:8080/fhir/ValueSet/S3D-FEST
